25f412eee5f5567a4b788e99f59d80c5809b170b
Shreynik jain <jainsy@rknec.edu>
2026-07-15 00:41:39 +0530

    Add files via upload

---
+++ PLSQL/Condition2.sql
@@ -0,0 +1,17 @@
+BEGIN
+    FOR cust IN (
+        SELECT CustomerID
+        FROM Customers
+        WHERE Balance > 10000
+    )
+    LOOP
+        UPDATE Customers
+        SET IsVIP = 'Y'
+        WHERE CustomerID = cust.CustomerID;
+    END LOOP;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('VIP customers updated successfully.');
+END;
+/
